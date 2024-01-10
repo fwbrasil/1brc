c780543fce8ee725bfff814133094ec64a0b29c8
Flavio Brasil <fwbrasil@gmail.com>
2024-01-10 13:58:05 -0800

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_fwbrasil.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_fwbrasil.java
@@ -19,9 +19,8 @@ import java.io.RandomAccessFile;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 import java.lang.reflect.Field;
+import java.nio.*;
 import java.nio.channels.FileChannel;
-import java.util.HashMap;
-import java.util.TreeMap;
 import java.util.concurrent.CountDownLatch;
 
 import sun.misc.Unsafe;
@@ -113,9 +112,9 @@ public final class CalculateAverage_fwbrasil {
         Results results = new Results();
 
         int id;
-        long start = 0L;
-        long end = 0L;
-        long pos = 0L;
+        long start = 0;
+        long end = 0;
+        long pos = 0;
 
         public void run(long start, long end) {
             this.start = start;
@@ -139,7 +138,7 @@ public final class CalculateAverage_fwbrasil {
             var keyEnd = pos;
             pos++;
             var value = readValue();
-            results.add(keyStart, keyEnd, hash, value);
+            results.add(hash, keyStart, keyEnd, value);
         }
 
         long readKeyHash() {
@@ -157,8 +156,8 @@ public final class CalculateAverage_fwbrasil {
             return r;
         }
 
-        int readValue() {
-            var value = Integer.MAX_VALUE;
+        short readValue() {
+            var value = 0;
             var negative = false;
             if (read(pos) == '-') {
                 negative = true;
@@ -168,89 +167,62 @@ public final class CalculateAverage_fwbrasil {
             if (maybeDot == '.') {
                 value = read(pos) * 10 + read(pos + 2) - 528;
                 pos += 3;
-            }
-            else {
+            } else {
                 value = read(pos) * 100 + maybeDot * 10 + read(pos + 3) - 5328;
                 pos += 4;
             }
-            return negative ? -value : value;
+            return (short) (negative ? -value : value);
         }
     }
 
     final class Results {
 
-        int idx = 0;
+        static final int FIELDS = 6;
+        static final long ENTRY_SIZE = FIELDS * TABLE_SCALE;
 
-        // keyHash | keyStart | keyEnd | 0 | min | max | sum | count
-        final long[] table = new long[TABLE_SIZE * 8];
+        // keyHash | keyStart | keyEnd | 0 | state(min, max, count) | sum
 
-        public void add(long keyStart, long keyEnd, long hash, int value) {
-            findKey(keyStart, keyEnd, hash);
-            var o = offset(4);
-            if (value < get(o)) {
-                put(o, value);
+        final ByteBuffer table = new DirectByteBuffer((int) (TABLE_SIZE * ENTRY_SIZE));
+
+        final long tableStart = table.;
+        final long tableEnd = tableStart + TABLE_SIZE * ENTRY_SIZE;
+
+        long offset = tableStart;
+        Key key = new Key();
+        State state = new State();
+
+        public void add(long keyHash, long keyStart, long keyEnd, short value) {
+            if (!findKey(keyHash)) {
+                key.init(keyHash, keyStart, keyEnd);
+                state.init(value);
+            } else {
+                state.update(value);
             }
-            o = next(o);
-            if (value > get(o)) {
-                put(o, value);
+        }
+
+        boolean findKey(long keyHash) {
+            long l = keyHash & TABLE_MASK;
+            offset = tableStart + l * FIELDS;
+            var h = 0L;
+            if ((h = UNSAFE.getLong(offset)) != keyHash && h != 0) {
+                next();
+                while ((h = UNSAFE.getLong(offset)) != keyHash && h != 0) {
+                    next();
+                }
             }
-            o = next(o);
-            put(o, get(o) + value);
-            o = next(o);
-            put(o, get(o) + 1);
+            return h != 0;
+        }
+
+        void next() {
+            var o = offset;
+            o += ENTRY_SIZE;
+            if (o > tableEnd) {
+                o = tableStart;
+            }
+            offset = o;
         }
 
         public void merge(Results other) {
-            for (int i = 0; i < TABLE_SIZE * 8; i += 8) {
-                other.idx = i;
-                if (!other.empty()) {
-                    idx = i;
-                    while (keyHash() != other.keyHash() && !empty()) {
-                        next();
-                    }
-                    if (empty()) {
-                        UNSAFE.copyMemory(other.table, other.offset(), table, offset(), TABLE_SCALE * 8);
-                    }
-                    else {
-                        var o1 = offset(4);
-                        var o2 = other.offset(4);
-                        var v = 0L;
-                        if ((v = other.get(o2)) < get(o1)) {
-                            put(o1, v);
-                        }
-                        o1 = next(o1);
-                        o2 = next(o2);
-                        if ((v = other.get(o2)) > get(o1)) {
-                            put(o1, v);
-                        }
-                        o1 = next(o1);
-                        o2 = next(o2);
-                        put(o1, get(o1) + other.get(o2));
-                        o1 = next(o1);
-                        put(o1, get(o1) + 1);
-                    }
-                }
-            }
-        }
-
-        @Override
-        public String toString() {
-            var results = new TreeMap<String, String>();
-            try {
-                for (int i = 0; i < TABLE_SIZE * 8; i += 8) {
-                    idx = i;
-                    if (!empty()) {
-                        var res = round(min() / 10.0) + "/" +
-                                round((sum() / 10.0) / count()) + "/"
-                                + round(max() / 10.0);
-                        results.put(readKey(), res);
-                    }
-                }
-            }
-            catch (Throwable ex) {
-                throw new RuntimeException(ex);
-            }
-            return results.toString();
         }
 
         double round(double value) {
@@ -258,95 +230,93 @@ public final class CalculateAverage_fwbrasil {
         }
 
         String readKey() {
-            var start = keyStart();
-            var size = (int) (keyEnd() - start);
+            var start = key.start;
+            var size = (int) (key.end - start);
             var bytes = new byte[size];
             UNSAFE.copyMemory(null, address + start, bytes, Unsafe.ARRAY_BYTE_BASE_OFFSET, size);
             try {
                 return new String(bytes, "UTF-8");
-            }
-            catch (Throwable ex) {
+            } catch (Throwable ex) {
                 throw new RuntimeException(ex);
             }
         }
 
-        void findKey(long keyStart, long keyEnd, long hash) {
-            idx = (int) (hash & TABLE_MASK) * 8;
-            if (keyHash() != hash && !empty()) {
-                next();
-                while (keyHash() != hash && !empty()) {
-                    next();
+        final class Key {
+            long hash = 0;
+            long start = 0;
+            long end = 0;
+
+            public void init(long hash, long start, long end) {
+                this.hash = hash;
+                this.start = start;
+                this.end = end;
+                write();
+            }
+
+            public void read() {
+                var o = offset;
+                hash = UNSAFE.getLong(o);
+                o += TABLE_SCALE;
+                start = UNSAFE.getLong(o);
+                o += TABLE_SCALE;
+                end = UNSAFE.getLong(o);
+            }
+
+            void write() {
+                var o = offset;
+                UNSAFE.putLong(o, hash);
+                o += TABLE_SCALE;
+                UNSAFE.putLong(o, start);
+                o += TABLE_SCALE;
+                UNSAFE.putLong(o, end);
+            }
+        }
+
+        final class State {
+            short min = Short.MAX_VALUE;
+            short max = Short.MIN_VALUE;
+            int count = 0;
+            long sum = 0;
+
+            public long init(short value) {
+                min = value;
+                max = value;
+                count = 1;
+                sum = value;
+                return write();
+            }
+
+            public long update(short value) {
+                read();
+                if (value < min) {
+                    min = value;
                 }
+                if (value > max) {
+                    max = value;
+                }
+                count++;
+                sum += value;
+                return write();
             }
-            if (empty()) {
-                var o = offset();
-                put(o, hash);
-                o = next(o);
-                put(o, keyStart);
-                o = next(o);
-                put(o, keyEnd);
-                o = next(o);
-                o = next(o);
-                put(o, Long.MAX_VALUE);
-                o = next(o);
-                put(o, Long.MIN_VALUE);
+
+            long write() {
+                var o = offset + 4;
+                long st = ((long) min << 48) | ((long) max << 32) | (count & 0xffffffffL);
+                UNSAFE.putLong(o, st);
+                o += TABLE_SCALE;
+                UNSAFE.putLong(o, sum);
+                return o + TABLE_SCALE;
             }
-        }
 
-        long get(long offset) {
-            return UNSAFE.getLong(table, offset);
-        }
-
-        void put(long offset, long v) {
-            UNSAFE.putLong(table, offset, v);
-        }
-
-        long next(long offset) {
-            return offset + TABLE_SCALE;
-        }
-
-        long offset() {
-            return offset(0);
-        }
-
-        long offset(long pos) {
-            return TABLE_BASE_OFFSET + ((idx + pos) * TABLE_SCALE);
-        }
-
-        void next() {
-            idx = (idx + 8) & TABLE_MASK;
-        }
-
-        boolean empty() {
-            return keyEnd() == 0;
-        }
-
-        long keyHash() {
-            return get(offset(0));
-        }
-
-        long keyStart() {
-            return get(offset(1));
-        }
-
-        long keyEnd() {
-            return get(offset(2));
-        }
-
-        long min() {
-            return get(offset(4));
-        }
-
-        long max() {
-            return get(offset(5));
-        }
-
-        long sum() {
-            return get(offset(6));
-        }
-
-        long count() {
-            return get(offset(7));
+            void read() {
+                var o = offset + 4;
+                var st = UNSAFE.getLong(o);
+                min = (short) (st >> 48);
+                max = (short) (st >> 32);
+                count = (int) st;
+                offset += TABLE_SCALE;
+                sum = UNSAFE.getLong(o);
+            }
         }
     }
 
@@ -355,8 +325,7 @@ public final class CalculateAverage_fwbrasil {
             Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
             theUnsafe.setAccessible(true);
             return (Unsafe) theUnsafe.get(Unsafe.class);
-        }
-        catch (NoSuchFieldException | IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
